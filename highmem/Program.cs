04bf63938a00fc403a1832ba1584208f74ad7070
Andrew Cavallo <65136929+Awire9966@users.noreply.github.com>
2023-11-13 20:44:34 -0500

    Add files via upload

---
+++ highmem/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace highmem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Byte[] bytes = new Byte[100000000];
+            for (int ii = 0; ii < 10; ii++)
+            {
+
+
+
+                for (int i = 0; i < 100000000; i++)
+                {
+                    bytes[i] = new Byte();
+                }
+            }
+            Console.Read();
+        }
+    }
+    public class Byte
+    {
+        byte main = 0x1;
+    }
+}
